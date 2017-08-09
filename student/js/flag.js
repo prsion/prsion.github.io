5bdda3b80690ee3e12da9ced37459f74d15efe67
prsion <prsion@mail.ru>
2017-08-09 17:40:52 +0300

    all
    already

---
+++ student/js/flag.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function(){
+// k=0;
+
+
+   // console.log("0000 вхождение"+k);
+   $('.PUSK ').click (function(){
+   $('.flag_li:not("first")').css("display","block");
+    // console.log("1 вхождение"+k);
+
+});
+        // console.log("2 вхождение"+k);
+
+     $('.flag_li:not("first")').click (function(){
+     // k=k+1;
+     // console.log("3 вхождение"+k);
+     $('.flag_li').css("display","none");
+     $('.flag_ul').prepend($(this).css("display","block"));
+
+     // console.log('FULL цикл прошел'+k);
+
+
+
+});
+
+ });
---
+++ student/js/forma.js
@@ -0,0 +1,15 @@
+﻿ function openBlock(el) {
+     var kids = el.parentNode.childNodes;
+     for (var k = 0; k < kids.length; k++) {
+         var child = kids[k];
+         if (child && child.className == "this_block_is_hidden") {
+             if (child.style.display != 'block') {
+                 child.style.display = 'block';
+             } else {
+                 child.style.display = 'none';
+             }
+         }
+     }
+ }
+
+
