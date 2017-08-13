59b8d9c9c7d97940ab2e57433ee24b8e8cef41de
prsion <prsion@mail.ru>
2017-08-13 10:22:08 +0300

    commit
    commit

--- student/js/flag.js
+++ student/js/flag.js
@@ -1,25 +1,25 @@
-﻿$(document).ready(function(){
+﻿$(document).ready(function obertka(){
 // k=0;
 
 
    // console.log("0000 вхождение"+k);
-   $('.PUSK ').click (function(){
-   $('.flag_li:not("first")').css("display","block");
+   $('.PUSK, li:first').click (function(){
+   $('.flag li:not("first")').css("display","block");
     // console.log("1 вхождение"+k);
 
-});
         // console.log("2 вхождение"+k);
 
-     $('.flag_li:not("first")').click (function(){
-     // k=k+1;
-     // console.log("3 вхождение"+k);
-     $('.flag_li').css("display","none");
-     $('.flag_ul').prepend($(this).css("display","block"));
+             $('.flag li:not("first")').click (function(){
+             // k=k+1;
+             // console.log("3 вхождение"+k);
+             $('.flag li').css("display","none");
+             $('.flag ul').prepend($(this).css("display","block"));
+             return obertka();
+             // console.log('FULL цикл прошел'+k);
 
-     // console.log('FULL цикл прошел'+k);
 
+        });
 
+    });
 
 });
-
- });
