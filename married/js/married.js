09f6729541354bbd65df4b5aa2809f9d053cf69d
prsion <prsion@mail.ru>
2019-02-20 14:06:26 +0200

    commit

---
+++ married/js/married.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+
+/*---------переключение меню---------*/
+     $('.menuToggle').click (function(){
+         $('.menu-main').slideToggle(300, function(){
+            if($(this).css('display')==='none'){
+              $(this).removeAttr('style');
+            }
+       });
+
+    });
+/*---конец ----переключение меню---------*/
+
+
+/*----slider slick-----*/
+  $('.sl').slick({
+	infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  /*dots: true,*/
+  arrows: false,
+  responsive: true
+  });
+
+
+/* ----- button home------ */
+$(window).scroll(function(){
+if ($(this).scrollTop() !=0) {
+$('.scrollup').fadeIn();
+} else {
+$('.scrollup').fadeOut();
+}
+});
+
+$('.scrollup').click(function(){
+$("html, body").animate({ scrollTop: 0 }, 800);
+return false;
+});
+
+
+
+
+});
