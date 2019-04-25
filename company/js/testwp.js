c765aa9a017e7f04670905e726a38facdc243a88
prsion <prsion@mail.ru>
2019-04-25 17:19:45 +0300

    commit
    commit

---
+++ company/js/testwp.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+
+/*---------переключение меню---------*/
+     $('.menuToggle').click (function(){
+         $('.menu-main').slideToggle(300, function(){
+            if($(this).css('display')==='none'){
+              $(this).removeAttr('style');
+            }
+       });
+
+    });
+/*---конец ----переключение меню---------*/
+
+
+
+/*----slider slick-----*/
+  $('.sl').slick({
+	infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: false,
+  autoplaySpeed: 2000,
+  dots: true,
+  arrows: true,
+  responsive: true,
+});
+
+
+
+
+
+/* ----- button home------ */
+$(window).scroll(function(){
+if ($(this).scrollTop() !=0) {
+$('.scrollup').fadeIn();
+} else {
+$('.scrollup').fadeOut();
+}
+});
+
+$('.scrollup').click(function(){
+$("html, body").animate({ scrollTop: 0 }, 800);
+return false;
+});
+
+
+
+});
+
+
+
