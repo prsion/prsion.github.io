cf38f59bdf37f121575d0292ee2796e3b96b7700
prsion <prsion@mail.ru>
2019-05-11 17:16:11 +0300

    prsion
    commit

--- company/js/testwp.js
+++ company/js/testwp.js
@@ -27,8 +27,6 @@ $(document).ready(function () {
 
 
 
-
-
 /* ----- button home------ */
 $(window).scroll(function(){
 if ($(this).scrollTop() !=0) {
@@ -43,9 +41,39 @@ $("html, body").animate({ scrollTop: 0 }, 800);
 return false;
 });
 
+/* mouse move  */
+
+const cards=document.querySelectorAll('.move');
+
+for (let i=0; i<cards.length; i++) {
+  const card=cards[i];
+  card.addEventListener('mousemove', startRotate);
+  card.addEventListener('mouseout', stopRotate);
+
+}
+
+function startRotate(e) {
+const cardItem = this.querySelector('.move-item');
+/*cardItem.style.transform = 'rotateX('+event.offsetY+'deg)';*/
+const halfHeight= cardItem.offsetHeight/2 ;
+cardItem.style.transform = 'rotateX('+-(event.offsetY-halfHeight)/3+'deg) rotateY('+(event.offsetX-halfHeight)/3+'deg)';
+/*cardItem.style.transform = 'rotateX(20deg)'*/
+
+}
+
+function stopRotate(e) {
+const cardItem = this.querySelector('.move-item');
+cardItem.style.transform = 'rotateX(0)';
+
+
+}
+
+
+
+
+
+
 
 
 });
-
-
 
