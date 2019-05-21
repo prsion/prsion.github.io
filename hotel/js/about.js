85833ea72cb5ad280f69d401054ca3bfc8e80689
prsion <prsion@mail.ru>
2019-05-21 10:28:59 +0300

    prsion
    commit

---
+++ hotel/js/about.js
@@ -0,0 +1,199 @@
+/* JS Document */
+
+/******************************
+
+[Table of Contents]
+
+1. Vars and Inits
+2. Set Header
+3. Init Menu
+4. Init Search
+5. Init Milestones
+
+
+
+******************************/
+
+$(document).ready(function()
+{
+	"use strict";
+
+	/*
+
+	1. Vars and Inits
+
+	*/
+
+	var map;
+	var header = $('.header');
+	var menu = $('.menu');
+	var menuActive = false;
+	var ctrl = new ScrollMagic.Controller();
+
+	setHeader();
+
+	$(window).on('resize', function()
+	{
+		setHeader();
+	});
+
+	$(document).on('scroll', function()
+	{
+		setHeader();
+	});
+
+	initMenu();
+	initSearch();
+	initMilestones();
+	initGoogleMap();
+
+	/*
+
+	2. Set Header
+
+	*/
+
+	function setHeader()
+	{
+		if($(window).scrollTop() > 91)
+		{
+			header.addClass('scrolled');
+		}
+		else
+		{
+			header.removeClass('scrolled');
+		}
+	}
+
+	/*
+
+	3. Init Menu
+
+	*/
+
+	function initMenu()
+	{
+		if($('.hamburger').length && $('.menu').length)
+		{
+			var hamb = $('.hamburger');
+
+			hamb.on('click', function()
+			{
+				if(!menuActive)
+				{
+					openMenu();
+				}
+				else
+				{
+					closeMenu();
+				}
+			});
+		}
+	}
+
+	function openMenu()
+	{
+		menu.addClass('active');
+		menuActive = true;
+	}
+
+	function closeMenu()
+	{
+		menu.removeClass('active');
+		menuActive = false;
+	}
+
+	/*
+
+	4. Init Search
+
+	*/
+
+	function initSearch()
+	{
+		if($('.search_dropdown').length)
+		{
+			var dds = $('.search_dropdown');
+			dds.each(function()
+			{
+				var dd = $(this);
+				if(dd.find('ul > li').length)
+				{
+					var ddItems = dd.find('ul > li');
+					dd.on('click', function()
+					{
+						dd.toggleClass('active');
+					});
+					ddItems.each(function()
+					{
+						var ddItem = $(this);
+						ddItem.on('click', function()
+						{
+							dd.find('span').text(ddItem.text());
+						});
+					});
+				}
+			});
+		}
+	}
+
+	/*
+
+	5. Init Milestones
+
+	*/
+
+	function initMilestones()
+	{
+		if($('.milestone_counter').length)
+		{
+			var milestoneItems = $('.milestone_counter');
+
+	    	milestoneItems.each(function(i)
+	    	{
+	    		var ele = $(this);
+	    		var endValue = ele.data('end-value');
+	    		var eleValue = ele.text();
+
+	    		/* Use data-sign-before and data-sign-after to add signs
+	    		infront or behind the counter number */
+	    		var signBefore = "";
+	    		var signAfter = "";
+
+	    		if(ele.attr('data-sign-before'))
+	    		{
+	    			signBefore = ele.attr('data-sign-before');
+	    		}
+
+	    		if(ele.attr('data-sign-after'))
+	    		{
+	    			signAfter = ele.attr('data-sign-after');
+	    		}
+
+	    		var milestoneScene = new ScrollMagic.Scene({
+		    		triggerElement: this,
+		    		triggerHook: 'onEnter',
+		    		reverse:false
+		    	})
+		    	.on('start', function()
+		    	{
+		    		var counter = {value:eleValue};
+		    		var counterTween = TweenMax.to(counter, 4,
+		    		{
+		    			value: endValue,
+		    			roundProps:"value",
+						ease: Circ.easeOut,
+						onUpdate:function()
+						{
+							document.getElementsByClassName('milestone_counter')[i].innerHTML = signBefore + counter.value + signAfter;
+						}
+		    		});
+		    	})
+			    .addTo(ctrl);
+	    	});
+		}
+	}
+
+
+
+});
---
+++ hotel/js/blog.js
@@ -0,0 +1,138 @@
+/* JS Document */
+
+/******************************
+
+[Table of Contents]
+
+1. Vars and Inits
+2. Set Header
+3. Init Menu
+4. Init Search
+
+
+******************************/
+
+$(document).ready(function()
+{
+	"use strict";
+
+	/*
+
+	1. Vars and Inits
+
+	*/
+
+	var map;
+	var header = $('.header');
+	var menu = $('.menu');
+	var menuActive = false;
+	var ctrl = new ScrollMagic.Controller();
+
+	setHeader();
+
+	$(window).on('resize', function()
+	{
+		setHeader();
+	});
+
+	$(document).on('scroll', function()
+	{
+		setHeader();
+	});
+
+	initMenu();
+	initSearch();
+	initGoogleMap();
+
+	/*
+
+	2. Set Header
+
+	*/
+
+	function setHeader()
+	{
+		if($(window).scrollTop() > 91)
+		{
+			header.addClass('scrolled');
+		}
+		else
+		{
+			header.removeClass('scrolled');
+		}
+	}
+
+	/*
+
+	3. Init Menu
+
+	*/
+
+	function initMenu()
+	{
+		if($('.hamburger').length && $('.menu').length)
+		{
+			var hamb = $('.hamburger');
+
+			hamb.on('click', function()
+			{
+				if(!menuActive)
+				{
+					openMenu();
+				}
+				else
+				{
+					closeMenu();
+				}
+			});
+		}
+	}
+
+	function openMenu()
+	{
+		menu.addClass('active');
+		menuActive = true;
+	}
+
+	function closeMenu()
+	{
+		menu.removeClass('active');
+		menuActive = false;
+	}
+
+	/*
+
+	4. Init Search
+
+	*/
+
+	function initSearch()
+	{
+		if($('.search_dropdown').length)
+		{
+			var dds = $('.search_dropdown');
+			dds.each(function()
+			{
+				var dd = $(this);
+				if(dd.find('ul > li').length)
+				{
+					var ddItems = dd.find('ul > li');
+					dd.on('click', function()
+					{
+						dd.toggleClass('active');
+					});
+					ddItems.each(function()
+					{
+						var ddItem = $(this);
+						ddItem.on('click', function()
+						{
+							dd.find('span').text(ddItem.text());
+						});
+					});
+				}
+			});
+		}
+	}
+
+
+});
